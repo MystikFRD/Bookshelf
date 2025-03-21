49f7f44a951e08b142b69035344b4a66803f6863
Mystik <mustafa.budak@fom-net.de>
2025-03-21 05:01:16 +0100

    d3.

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,4 @@
-//import "./diagnostic.jsx";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import router from './router.jsx'; // Deine Routen-Konfiguration
-import { Provider } from 'react-redux';
-import store from './Store/store.jsx'; // Dein Redux-Store
-import ErrorBoundary from "./Components/ErrorBoundary.jsx";
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router}>
-                <App />
-            </RouterProvider>
-        </Provider>
-    </StrictMode>
-);
-createRoot(document.getElementById('root')).render(
-  <ErrorBoundary>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </ErrorBoundary>
-);
+// In main.jsx
+import FixedNavbar from './Components/FixedNavbar';
 
+// ... rest of your code
