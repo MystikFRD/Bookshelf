228ea00e950c045788837a0edab2bb612d6a8ab9
Mystik <mustafa.budak@fom-net.de>
2025-03-21 05:55:46 +0100

    Not really working properly but A LOT BETTER THAN BEFORE

--- src/Components/Booksdata.jsx
+++ src/Components/Booksdata.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import BookCard from './BookCard'; // Importiere die BookCard-Komponente
+import BookCard from './BookCard';
 
-const Booksdata = ({ title, books }) => {
+const Booksdata = ({ title = "Featured Books", books = [] }) => {
     return (
         <div>
             <h2 className="text-xl font-bold mb-4">{title}</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {books.length > 0 ? (
-                    books.map((book) => <BookCard key={book.key} book={book} />)
+                {books && books.length > 0 ? (
+                    books.map((book) => <BookCard key={book.key || book.id} book={book} />)
                 ) : (
                     <p>Keine Bücher gefunden.</p>
                 )}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+import { useSelector } from "react-redux";
 import Categories from "../../Components/Categories";
 import Booksdata from "../../Components/Booksdata";
 import hero_image from '../../assets/hero_images.png';
-import Footer from '../../Components/Footer';
 
 const Home = () => {
+    // Get books from Redux store
+    const books = useSelector(state => state.book.books);
+
     return (
         <div>
             <section id="hero_Section" className="bg-bgBanner flex md:flex-row flex-col md:justify-center justify-start items-start md:items-center w-full p-5 gap-10">
@@ -13,8 +17,9 @@ const Home = () => {
                 <img src={hero_image} alt="hero_image" className='w-96 h-auto' />
             </section>
             <Categories />
-            <Booksdata />
-            <Footer />
+            <Booksdata title="Featured Books" books={books} />
         </div>
-    )}
+    )
+}
+
 export default Home;
